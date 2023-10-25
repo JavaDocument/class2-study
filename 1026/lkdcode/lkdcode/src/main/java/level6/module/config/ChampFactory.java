b7da340a5d7ca95cc279e854aeac3eba339845da
lkdcode <dlrlejr1@naver.com>
2023-10-25 22:19:07 +0900

    Add : ChampFactory init

---
+++ 1026/lkdcode/lkdcode/src/main/java/level6/module/config/ChampFactory.java
@@ -0,0 +1,14 @@
+package level6.module.config;
+
+import level6.module.domain.character.inter.Champ;
+
+public class ChampFactory {
+
+    private ChampFactory() {
+    }
+
+    public static Champ createWarrior(final String command) {
+        return null;
+    }
+
+}
