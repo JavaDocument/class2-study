7c7a7cdd4e54c46ea1392fc08efd2fbf21fe5825
lkdcode <dlrlejr1@naver.com>
2023-10-26 17:14:27 +0900

    Add : PostResponse

---
+++ 1026/lkdcode/lkdcode/src/main/java/level8/domain/controller/common/PostResponse.java
@@ -0,0 +1,30 @@
+package level8.domain.controller.common;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+
+@JsonPropertyOrder({"resultCode", "msg", "data"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+public class PostResponse<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
+
+    public PostResponse(String resultCode, String msg, T data) {
+        this.resultCode = resultCode;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static <T> PostResponse<T> ok(T data) {
+        return new PostResponse<>("S-1", "성공", data);
+    }
+
+    public static <T> PostResponse<T> error(T data) {
+        return new PostResponse<>("S-1", "실패", data);
+    }
+
+}
