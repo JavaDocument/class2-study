fa660cdd92b1e7da75262f614119069082111f96
lkdcode <dlrlejr1@naver.com>
2023-10-26 10:06:55 +0900

    Refactor : Implement changes in abstract class and introduce constructor pattern for Wizard class

--- 1026/lkdcode/lkdcode/src/main/java/level6/module/domain/character/impl/Wizard.java
+++ 1026/lkdcode/lkdcode/src/main/java/level6/module/domain/character/impl/Wizard.java
@@ -1,18 +1,33 @@
 package level6.module.domain.character.impl;
 
 import level6.module.domain.character.abs.AbstractChamp;
-import level6.module.domain.weapon.impl.FireWand;
 import level6.module.domain.weapon.inter.Weapon;
 
 public class Wizard extends AbstractChamp {
+    private static final int DAMAGE = 5;
+    private Weapon weapon;
+
+    private Wizard(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public static Wizard from(Weapon weapon) {
+        return new Wizard(weapon);
+    }
+
+    @Override
+    public void changeWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
 
     @Override
     public int getChampDamage() {
-        return 5;
+        return DAMAGE;
     }
 
     @Override
-    protected Weapon initializeWeapon() {
-        return new FireWand();
+    protected Weapon getChampWeapon() {
+        return weapon;
     }
+
 }
