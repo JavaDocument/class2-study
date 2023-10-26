2def3b07f988cc997b810d93cd17165ef31bcfea
lkdcode <dlrlejr1@naver.com>
2023-10-26 10:07:43 +0900

    Refactor : main method

--- 1026/lkdcode/lkdcode/src/main/java/level6/Application.java
+++ 1026/lkdcode/lkdcode/src/main/java/level6/Application.java
@@ -1,23 +1,32 @@
 package level6;
 
-import level6.module.domain.character.impl.Warrior;
-import level6.module.domain.character.impl.Wizard;
+import level6.module.config.ChampFactory;
 import level6.module.domain.character.inter.Champ;
 import level6.module.domain.weapon.impl.Bow;
+import level6.module.domain.weapon.impl.FireWand;
+import level6.module.domain.weapon.impl.IceWand;
 import level6.module.domain.weapon.impl.Sword;
 
 public class Application {
     public static void main(String[] args) {
-        Champ champ = new Warrior();
-        champ.attack();
+        Champ swordWarrior = ChampFactory.createChampFrom("칼전사");
+        swordWarrior.attack();
+        swordWarrior.changeWeapon(Bow.newInstance());
+        swordWarrior.attack();
 
-        champ.changeWeapon(new Bow());
-        champ.attack();
+        Champ bowWarrior = ChampFactory.createChampFrom("활전사");
+        bowWarrior.attack();
+        bowWarrior.changeWeapon(Sword.newInstance());
+        bowWarrior.attack();
 
-        Champ champ1 = new Wizard();
-        champ1.attack();
+        Champ fireWizard = ChampFactory.createChampFrom("불마법사");
+        fireWizard.attack();
+        fireWizard.changeWeapon(IceWand.newInstance());
+        fireWizard.attack();
 
-        champ1.changeWeapon(new Sword());
-        champ1.attack();
+        Champ iceWizard = ChampFactory.createChampFrom("얼음마법사");
+        iceWizard.attack();
+        iceWizard.changeWeapon(FireWand.newInstance());
+        iceWizard.attack();
     }
 }
