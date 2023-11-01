a52ea60ab7456f0add8dd7545ec1f0c8a0234c6e
JeFeel <jaephil94@gmail.com>
2023-11-02 06:12:35 +0900

    Build Spring project

---
+++ springboot-practice/jefeel/src/test/java/com/practice/jefeel/JefeelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jefeel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JefeelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
