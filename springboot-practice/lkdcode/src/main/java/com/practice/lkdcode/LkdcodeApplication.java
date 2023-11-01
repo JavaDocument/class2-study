cfd5195c0615a066862af23bf69bc040c0bc0ce7
lkdcode <dlrlejr1@naver.com>
2023-11-01 17:01:59 +0900

    Build : initialize

---
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/LkdcodeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.lkdcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkdcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkdcodeApplication.class, args);
+	}
+
+}
---
+++ springboot-practice/lkdcode/src/test/java/com/practice/lkdcode/LkdcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.lkdcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkdcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
