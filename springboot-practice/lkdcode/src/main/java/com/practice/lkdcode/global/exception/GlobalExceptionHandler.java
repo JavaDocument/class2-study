e778ba894e187c87673507111b8f43439afd28f8
lkdcode <dlrlejr1@naver.com>
2023-11-05 23:40:26 +0900

    Add [GlobalExceptionHandler] :  Add @Valid Exception

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/global/exception/GlobalExceptionHandler.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.practice.lkdcode.global.exception;
 
 import com.practice.lkdcode.global.response.PostResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +11,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public PostResponse<?> catchIllegalArgumentException(IllegalArgumentException e) {
         return PostResponse.error(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public PostResponse<?> catchMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return PostResponse.error(e.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage());
     }
 
 }
