6e9d0d801e6b3ba5b4b719cf59f4102521214d5f
JeFeel <jaephil94@gmail.com>
2023-11-08 23:52:06 +0900

    Add Post : entity

---
+++ springboot-practice/jefeel/src/main/java/com/practice/jefeel/post/domain/entity/Post.java
@@ -0,0 +1,35 @@
+package com.practice.jefeel.post.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+}
