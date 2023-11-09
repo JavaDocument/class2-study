08ee1dc9ac27295f25b75043dc8238a774b0a2a7
lkdcode <dlrlejr1@naver.com>
2023-11-09 12:34:07 +0900

    Test [PostApiControllerTest] : PostApiControllerTest
    1. 게시글_ID_조회_성공_테스트
    2. 게시글_ID_조회_실패_테스트
    3. 게시글_수정_성공_테스트
    4. 게시글_수정_실패_테스트

--- springboot-practice/lkdcode/src/test/java/com/practice/lkdcode/module/post/controller/PostApiControllerTest.java
+++ springboot-practice/lkdcode/src/test/java/com/practice/lkdcode/module/post/controller/PostApiControllerTest.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.lkdcode.module.post.controller.dto.request.PostRequestDTO;
 import com.practice.lkdcode.module.post.domain.Post;
 import com.practice.lkdcode.module.post.domain.repository.PostRepository;
+import com.practice.lkdcode.module.post.exception.PostErrorCode;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,23 +16,26 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class PostApiControllerTest {
 
     @Autowired
@@ -110,16 +116,7 @@ class PostApiControllerTest {
     void 게시글_전체_조회_성공_테스트() throws Exception {
         // given
         final String url = "/posts";
-
-        String title = "Junit title";
-        String content = "Junit content";
-
-        for (int i = 0; i < 10; i++) {
-            postRepository.save(Post.builder()
-                    .title(title + i)
-                    .content(content + i)
-                    .build());
-        }
+        setPostBulkData();
 
         // when
         MvcResult mvcResult = mockMvc.perform(get(url)
@@ -138,8 +135,133 @@ class PostApiControllerTest {
         JsonNode jsonNode = objectMapper.readTree(contentAsString);
         String s = jsonNode.at("/data/1/content").asText();
 
-        Assertions.assertThat(s)
+        assertThat(s)
                 .isNotEqualTo("테스트 content8");
+    }
+
+    @Test
+    void 게시글_ID_조회_성공_테스트() throws Exception {
+        // given
+        final String id = "3";
+        final String url = "/posts/" + id;
+        setPostBulkData();
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(get(url)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.id").exists())
+                .andExpect(jsonPath("$.data.title").exists())
+                .andExpect(jsonPath("$.data.content").exists())
+                .andReturn();
+
+        // then
+        String content = mvcResult.getResponse().getContentAsString();
+        JsonNode jsonNode = objectMapper.readTree(content);
+        String jsonId = jsonNode.at("/data/id").asText();
+
+        assertThat(id)
+                .isEqualTo(jsonId);
+    }
+
+    @Test
+    void 게시글_ID_조회_실패_테스트() throws Exception {
+        // given
+        final String id = "3000";
+        final String url = "/posts/" + id;
+        setPostBulkData();
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(get(url)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                ).andExpect(status().isBadRequest())
+                .andReturn();
+
+        // then
+        String errorMessage = mvcResult.getResponse().getContentAsString();
+        assertThat(errorMessage)
+                .isEqualTo(PostErrorCode.NOT_FOUND_POST_BY_ID_ERROR.getMessage() + id);
+    }
+
+    @Test
+    void 게시글_수정_성공_테스트() throws Exception {
+        // given
+        final String updateTitle = "수정한 제목";
+        final String updateContent = "수정한 내용";
+
+        final String id = "3";
+        final String url = "/posts/" + id;
+
+        setPostBulkData();
+
+        PostRequestDTO.Update request = PostRequestDTO.Update.builder()
+                .title(updateTitle)
+                .content(updateContent)
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(request);
+        // when
+
+        MvcResult mvcResult = mockMvc.perform(patch(url)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+                ).andExpect(status().isOk())
+                .andReturn();
+
+        // then
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        JsonNode jsonNode = objectMapper.readTree(contentAsString);
+        String updatedTitle = jsonNode.at("/data/title").asText();
+        String updatedContent = jsonNode.at("/data/content").asText();
+
+        Assertions.assertThat(updateTitle)
+                .isEqualTo(updatedTitle);
+
+        Assertions.assertThat(updateContent)
+                .isEqualTo(updatedContent);
+    }
+
+    @Test
+    void 게시글_수정_실패_테스트() throws Exception {
+        // given
+        final String errorUpdateTitle = "         ";
+        final String updateContent = "수정한 내용";
+
+        final String id = "3";
+        final String url = "/posts/" + id;
+
+        setPostBulkData();
+
+        PostRequestDTO.Update request = PostRequestDTO.Update.builder()
+                .title(errorUpdateTitle)
+                .content(updateContent)
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(request);
+        // when
+
+        MvcResult mvcResult = mockMvc.perform(patch(url)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+                ).andExpect(status().isBadRequest())
+                .andReturn();
+
+        // then
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        Assertions.assertThat(contentAsString)
+                .isEqualTo("수정할 제목을 입력해주세요");
+    }
+
+    private void setPostBulkData() {
+        String title = "Junit title";
+        String content = "Junit content";
+
+        for (int i = 0; i < 10; i++) {
+            postRepository.save(Post.builder()
+                    .title(title + i)
+                    .content(content + i)
+                    .build());
+        }
     }
 
 }
