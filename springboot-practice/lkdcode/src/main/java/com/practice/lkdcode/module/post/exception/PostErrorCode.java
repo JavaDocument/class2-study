992129b095eac89a3882f98f8da3bcfc45a8044d
lkdcode <dlrlejr1@naver.com>
2023-11-09 11:27:54 +0900

    Add [PostErrorCode] : NOT_FOUND_POST_BY_ID_ERROR

---
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/exception/PostErrorCode.java
@@ -0,0 +1,17 @@
+package com.practice.lkdcode.module.post.exception;
+
+import com.practice.lkdcode.global.exception.AppErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@RequiredArgsConstructor
+@Getter
+public enum PostErrorCode implements AppErrorCode {
+    NOT_FOUND_POST_BY_ID_ERROR("존재하지 않는 게시글입니다. Id : ", HttpStatus.BAD_REQUEST),
+    ;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+}
