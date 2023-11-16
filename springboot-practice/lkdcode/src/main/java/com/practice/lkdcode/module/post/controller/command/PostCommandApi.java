640e8d0810d696b37ec55b4ef75046a02f37a5d9
lkdcode <dlrlejr1@naver.com>
2023-11-16 17:33:14 +0900

    Add [PostCommandApi] : initial commit

---
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/controller/command/PostCommandApi.java
@@ -0,0 +1,11 @@
+package com.practice.lkdcode.module.post.controller.command;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostCommandApi {
+}
