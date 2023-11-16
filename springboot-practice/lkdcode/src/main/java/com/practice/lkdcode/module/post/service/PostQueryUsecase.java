6f3f0a75e4fc71199e429e01fabade712b74b115
lkdcode <dlrlejr1@naver.com>
2023-11-16 18:25:07 +0900

    Add [PostQueryUsecase] : PostQueryUsecase

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/service/PostQueryUsecase.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/service/PostQueryUsecase.java
@@ -1,5 +1,14 @@
 package com.practice.lkdcode.module.post.service;
 
-public interface PostQueryUsecase {
+import com.practice.lkdcode.module.post.controller.dto.response.PostResponseDTO;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
+public interface PostQueryUsecase {
+    PostResponseDTO.Get retrieveFindById(final Long id);
+
+    List<PostResponseDTO.Get> retrieveFindAll(Pageable pageable);
+
+    List<PostResponseDTO.Get> retrieveFindByTitleContaining(String keyword, Pageable pageable);
 }
