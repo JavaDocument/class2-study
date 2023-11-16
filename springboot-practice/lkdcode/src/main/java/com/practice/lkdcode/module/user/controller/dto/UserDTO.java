94d5558291bbd97c41201123f3245c6f377d9332
lkdcode <dlrlejr1@naver.com>
2023-11-16 17:36:42 +0900

    Add [UserDTO] : UserDTO

---
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/controller/dto/UserDTO.java
@@ -0,0 +1,46 @@
+package com.practice.lkdcode.module.user.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+
+import javax.validation.constraints.*;
+
+import static com.practice.lkdcode.module.user.controller.dto.UserDTO.*;
+
+public sealed interface UserDTO permits UserSignupRequestDTO, UserSignupResponseDTO, UserSignInRequestDTO, UserSignInResponseDTO {
+    @Builder
+    record UserSignupRequestDTO(
+            @NotBlank(message = "이메일을 입력해주세요.")
+            @Email(message = "이메일 형식에 맞지 않습니다.")
+            String email,
+            @NotBlank(message = "비밀번호를 입력해주세요.")
+            @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하여야합니다.")
+            String password
+    ) implements UserDTO {
+    }
+
+    @Builder
+    record UserSignupResponseDTO(
+            String email,
+            boolean success
+    ) implements UserDTO {
+    }
+
+    @Builder
+    record UserSignInRequestDTO(
+            @NotBlank(message = "이메일을 입력해주세요.")
+            @Email(message = "이메일 형식에 맞지 않습니다.")
+            String email,
+            @NotBlank(message = "비밀번호를 입력해주세요.")
+            @Size(min = 8, max = 15, message = "비밀번호는 8자 이상 15자 이하여야합니다.")
+            String password
+    ) implements UserDTO {
+    }
+
+    @Builder
+    record UserSignInResponseDTO(
+            @JsonInclude(JsonInclude.Include.NON_EMPTY)
+            String token
+    ) implements UserDTO {
+    }
+}
