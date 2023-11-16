0672eecb3e1f576cf2d6cf7877f2f6a8a5f2f7ef
lkdcode <dlrlejr1@naver.com>
2023-11-16 18:24:43 +0900

    Add [PostCommandUsecase] : PostCommandUsecase

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/service/PostCommandUsecase.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/service/PostCommandUsecase.java
@@ -1,5 +1,13 @@
 package com.practice.lkdcode.module.post.service;
 
-public interface PostCommandUsecase {
+import com.practice.lkdcode.global.config.security.CustomUserDetails;
+import com.practice.lkdcode.module.post.controller.dto.request.PostRequestDTO;
+import com.practice.lkdcode.module.post.controller.dto.response.PostResponseDTO;
 
+public interface PostCommandUsecase {
+    PostResponseDTO.Create executeSave(final PostRequestDTO.Create request, CustomUserDetails customUserDetails);
+
+    PostResponseDTO.Update executeUpdate(final Long id, final PostRequestDTO.Update request, CustomUserDetails customUserDetails);
+
+    PostResponseDTO.Delete executeDelete(final Long id, CustomUserDetails customUserDetails);
 }
