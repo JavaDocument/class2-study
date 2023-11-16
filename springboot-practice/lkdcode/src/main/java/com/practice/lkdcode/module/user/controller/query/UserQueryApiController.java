196271ecfed4a9d0f3bfd64ac9102ca9b8e8e982
lkdcode <dlrlejr1@naver.com>
2023-11-16 18:25:45 +0900

    Add [UserQueryApiController] : UserQueryApiController

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/controller/query/UserQueryApiController.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/controller/query/UserQueryApiController.java
@@ -1,4 +1,23 @@
 package com.practice.lkdcode.module.user.controller.query;
 
+import com.practice.lkdcode.global.config.security.CustomUserDetails;
+import com.practice.lkdcode.module.user.controller.dto.UserDTO;
+import com.practice.lkdcode.module.user.service.UserQueryUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserQueryApiController {
+    private final UserQueryUsecase userQueryUsecase;
+
+
+    @GetMapping
+    public UserDTO.UserInformation getUserInformation(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return userQueryUsecase.retrieveUserInformation(customUserDetails.getId());
+    }
 }
