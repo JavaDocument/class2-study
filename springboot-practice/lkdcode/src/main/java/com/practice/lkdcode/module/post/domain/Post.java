4b416fe659b01e0a9070c786dbee4906d3e0abe6
lkdcode <dlrlejr1@naver.com>
2023-11-16 17:32:13 +0900

    Add [Post] : 테이블 이름 명시,

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/domain/Post.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/domain/Post.java
@@ -1,20 +1,18 @@
 package com.practice.lkdcode.module.post.domain;
 
 import com.practice.lkdcode.global.common.BaseEntity;
-import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_post")
 public class Post extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
     @Column(name = "title", nullable = false, length = 100)
     private String title;
     @Column(columnDefinition = "TEXT", name = "content", nullable = false, length = 1_000)
@@ -30,5 +28,4 @@ public class Post extends BaseEntity {
         this.title = title;
         this.content = content;
     }
-
 }
