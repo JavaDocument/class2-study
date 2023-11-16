4332d5b527d33fdbae6a5d0e93d8a05f0f4ea283
lkdcode <dlrlejr1@naver.com>
2023-11-16 17:31:51 +0900

    Add [JWTProvider] : JWTProvider

---
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/global/config/jwt/JWTProvider.java
@@ -0,0 +1,83 @@
+package com.practice.lkdcode.global.config.jwt;
+
+import com.practice.lkdcode.global.config.security.CustomUserDetails;
+import com.practice.lkdcode.module.user.domain.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.lang.Assert;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class JWTProvider {
+    private static final String ID = "id";
+    private final JWTProperties jwtProperties;
+
+    // 토큰 생성
+    public String generateToken(User user) {
+        Date now = new Date();
+
+        Date expiration = new Date();
+        expiration.setTime(expiration.getTime() + 1_800_000);
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 type : JWT
+                .setIssuer(jwtProperties.getIssuer()) // 내용 iss : yml 에서 설정한 값
+                .setIssuedAt(now) // 내용 iat : 현재 시간
+                .setExpiration(expiration) // 내용 exp : expiry 멤버 변수값
+                .setSubject(user.getEmail()) // 내용 sub : 유저의 이메일
+                .claim(ID, user.getId()) // 클레임 id : 유저 ID
+                // 서명 : 비밀값과 함께 해시값을 HS256 방식으로 암호화
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    // 토큰 유효성 검사
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // CustomUserDetails 취득
+
+    public CustomUserDetails getCustomUserDetails(final String token) {
+        Assert.hasText(token, "token parameter must not be empty or null");
+
+        return new CustomUserDetails(getUserId(token));
+    }
+
+    // 토큰 기반으로 유저 인증 정보 취득
+    public UsernamePasswordAuthenticationToken getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UsernamePasswordAuthenticationToken(
+                new org.springframework.security.core.userdetails.User
+                        (claims.getSubject(), "", authorities), token, authorities);
+    }
+
+    // 토큰 기반으로 유저 아이디 취득
+    public Long getUserId(String token) {
+        Claims claims = getClaims(token);
+        return claims.get(ID, Long.class);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
