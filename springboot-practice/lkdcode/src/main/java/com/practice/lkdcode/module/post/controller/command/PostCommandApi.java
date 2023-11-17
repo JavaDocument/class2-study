fa399f6d2177c9bd9f2b85e1195b37aa06b78b32
lkdcode <dlrlejr1@naver.com>
2023-11-17 17:58:04 +0900

    Add [PostCommandApi] : 메서드 접두사 통일

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/controller/command/PostCommandApi.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/controller/command/PostCommandApi.java
@@ -4,7 +4,7 @@ import com.practice.lkdcode.global.config.security.CustomUserDetails;
 import com.practice.lkdcode.module.post.controller.dto.request.PostRequestDTO;
 import com.practice.lkdcode.module.post.controller.dto.response.PostResponseDTO;
 import com.practice.lkdcode.module.post.controller.response.PostResponse;
-import com.practice.lkdcode.module.post.service.PostCommandUsecase;
+import com.practice.lkdcode.module.post.service.command.PostCommandUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +19,7 @@ public class PostCommandApi {
     private final PostCommandUsecase postCommandUsecase;
 
     @PostMapping
-    public PostResponse<PostResponseDTO.Create> createPost(
+    public PostResponse<PostResponseDTO.Create> getCreatePost(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody @Valid final PostRequestDTO.Create request
     ) {
@@ -28,7 +28,7 @@ public class PostCommandApi {
     }
 
     @DeleteMapping("/{id}")
-    public PostResponse<PostResponseDTO.Delete> deletePost(
+    public PostResponse<PostResponseDTO.Delete> getDeletePost(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @PathVariable(name = "id") final Long id
     ) {
@@ -37,7 +37,7 @@ public class PostCommandApi {
     }
 
     @PatchMapping("/{id}")
-    public PostResponse<PostResponseDTO.Update> updatePost(
+    public PostResponse<PostResponseDTO.Update> getUpdatePost(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @PathVariable(name = "id") final Long id,
             @RequestBody @Valid final PostRequestDTO.Update request
