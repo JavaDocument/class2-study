b09eac902334cec32ca2cb672c94a64bcad10e92
lkdcode <dlrlejr1@naver.com>
2023-11-17 18:05:50 +0900

    Add [PostResponseDTO] : LocalDateTime 직렬화

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/controller/dto/response/PostResponseDTO.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/controller/dto/response/PostResponseDTO.java
@@ -1,9 +1,14 @@
 package com.practice.lkdcode.module.post.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.practice.lkdcode.module.user.domain.User;
 import lombok.Builder;
 
+import javax.persistence.JoinColumn;
 import java.time.LocalDateTime;
 
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
 import static com.practice.lkdcode.module.post.controller.dto.response.PostResponseDTO.*;
 
 public sealed interface PostResponseDTO permits Create, Update, Delete, Get {
@@ -12,18 +17,25 @@ public sealed interface PostResponseDTO permits Create, Update, Delete, Get {
             Long id,
             String title,
             String content,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
             LocalDateTime createdAt,
-            LocalDateTime updatedAt
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime updatedAt,
+            Long userId
     ) implements PostResponseDTO {
     }
 
     @Builder
+    @JsonInclude
     record Update(
             Long id,
             String title,
             String content,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
             LocalDateTime createdAt,
-            LocalDateTime updatedAt
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime updatedAt,
+            Long userId
 
     ) implements PostResponseDTO {
     }
@@ -33,8 +45,11 @@ public sealed interface PostResponseDTO permits Create, Update, Delete, Get {
             Long id,
             String title,
             String content,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
             LocalDateTime createdAt,
-            LocalDateTime updatedAt
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime updatedAt,
+            Long userId
     ) implements PostResponseDTO {
     }
 
@@ -43,8 +58,11 @@ public sealed interface PostResponseDTO permits Create, Update, Delete, Get {
             Long id,
             String title,
             String content,
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
             LocalDateTime createdAt,
-            LocalDateTime updatedAt
+            @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "Asia/Seoul")
+            LocalDateTime updatedAt,
+            Long userId
     ) implements PostResponseDTO {
     }
 
