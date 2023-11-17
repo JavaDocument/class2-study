0a838654be8b20a949e66de64e4e76740d005ca0
lkdcode <dlrlejr1@naver.com>
2023-11-17 17:59:57 +0900

    Chore [PostNotFoundByIdException] : 패키지 변경

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/exception/custom/PostNotFoundByIdException.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/exception/custom/PostNotFoundByIdException.java
@@ -1,7 +1,7 @@
 package com.practice.lkdcode.module.post.exception.custom;
 
-import com.practice.lkdcode.global.exception.AppErrorCode;
-import com.practice.lkdcode.global.exception.AppException;
+import com.practice.lkdcode.global.exception.custom.AppErrorCode;
+import com.practice.lkdcode.global.exception.custom.AppException;
 import lombok.Getter;
 
 public class PostNotFoundByIdException extends AppException {
