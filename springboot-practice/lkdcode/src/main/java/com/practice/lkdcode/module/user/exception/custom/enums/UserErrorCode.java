bc4fd914d0d985256a6697d39ad4776ac107e4e4
lkdcode <dlrlejr1@naver.com>
2023-11-17 18:03:53 +0900

    Add [UserErrorCode] : UserErrorCode

---
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/exception/custom/enums/UserErrorCode.java
@@ -0,0 +1,17 @@
+package com.practice.lkdcode.module.user.exception.custom.enums;
+
+import com.practice.lkdcode.global.exception.custom.AppErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements AppErrorCode {
+    LOGIN_FAIL_ERROR("아이디 혹은 비밀번호를 다시 확인해주세요.", HttpStatus.BAD_REQUEST),
+    USER_EMAIL_DUPLICATION_ERROR("아이디가 중복되었습니다.", HttpStatus.BAD_REQUEST),
+    ;
+
+    private final String message;
+    private final HttpStatus httpStatus;
+}
