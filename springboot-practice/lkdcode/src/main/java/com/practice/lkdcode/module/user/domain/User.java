b8753fb71b574dfa32fe6e18b27008c317f643b9
lkdcode <dlrlejr1@naver.com>
2023-11-17 18:05:33 +0900

    Add [User] : Post N : 1 User, BaseEntity 제거

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/domain/User.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/domain/User.java
@@ -1,24 +1,22 @@
 package com.practice.lkdcode.module.user.domain;
 
 import com.practice.lkdcode.global.common.BaseEntity;
+import com.practice.lkdcode.module.post.domain.Post;
 import com.practice.lkdcode.module.user.domain.status.UserStatus;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "tb_user")
-public class User extends BaseEntity implements UserDetails {
+public class User extends BaseEntity {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
     @Column(name = "password", nullable = false)
@@ -26,44 +24,13 @@ public class User extends BaseEntity implements UserDetails {
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
+
     @Builder
     public User(String email, String password, UserStatus userStatus) {
         this.email = email;
         this.password = password;
-        this.userStatus = userStatus;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("user"));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        // 계정 만료 여부
-        return true; // true -> 만료되지 않았음
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        // 계정 잠금 여부
-        return false; // true -> 잠금되지 않았음
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        // 비밀번호 만료 여부
-        return true; // true -> 만료되지 않았음
-    }
-
-    @Override
-    public boolean isEnabled() {
-        // 계정 사용 가능 여부
-        return false; // true -> 사용 가능
+        this.userStatus = userStatus == null ? UserStatus.CREATED : userStatus;
     }
 }
