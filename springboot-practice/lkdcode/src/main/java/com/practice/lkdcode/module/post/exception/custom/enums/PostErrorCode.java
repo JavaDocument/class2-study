0eb545a3c4521f835dded1e36f4a6cc28703020e
lkdcode <dlrlejr1@naver.com>
2023-11-18 21:25:01 +0900

    Add [PostErrorCode] : 본인 글 수정/삭제 에러

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/exception/custom/enums/PostErrorCode.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/exception/custom/enums/PostErrorCode.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum PostErrorCode implements AppErrorCode {
     NOT_FOUND_POST_BY_ID_ERROR("존재하지 않는 게시글입니다. Id : ", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED_POST_UPDATE_ERROR("본인 글만 업데이트할 수 있습니다.", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED_POST_DELETE_ERROR("본인 글만 삭제할 수 있습니다.", HttpStatus.BAD_REQUEST),
     ;
     private final String message;
     private final HttpStatus httpStatus;
