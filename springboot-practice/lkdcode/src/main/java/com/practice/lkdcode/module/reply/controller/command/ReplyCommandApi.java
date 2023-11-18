7e31bf20059889d84e7fcc0625ec1c23548fc6ff
lkdcode <dlrlejr1@naver.com>
2023-11-18 21:26:05 +0900

    Add [ReplyCommandApi] : ReplyCommandApi

---
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/reply/controller/command/ReplyCommandApi.java
@@ -0,0 +1,39 @@
+package com.practice.lkdcode.module.reply.controller.command;
+
+import com.practice.lkdcode.global.config.security.CustomUserDetails;
+import com.practice.lkdcode.module.reply.controller.dto.request.ReplyRequestDTO;
+import com.practice.lkdcode.module.reply.controller.dto.response.ReplyResponseDTO;
+import com.practice.lkdcode.module.reply.service.command.ReplyCommandUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/replies")
+@RequiredArgsConstructor
+public class ReplyCommandApi {
+    private final ReplyCommandUsecase replyCommandUsecase;
+
+    @PostMapping("/{postId}")
+    public ReplyResponseDTO.CreateResponseDTO create(
+            @PathVariable(name = "postId") Long postId,
+            @RequestBody @Valid ReplyRequestDTO.CreateRequestDTO dto,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        return replyCommandUsecase.executeSave(postId, dto, customUserDetails);
+    }
+
+    @PatchMapping
+    public void update(
+
+    ) {
+    }
+
+    @DeleteMapping
+    public void delete(
+
+    ) {
+    }
+}
