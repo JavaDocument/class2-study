33be368b9bba80eb93b805d1a0fd0cd61f68bb68
lkdcode <dlrlejr1@naver.com>
2023-11-18 21:29:00 +0900

    Add [UserErrorCode] : NOT_FOUND_USER_BY_ID_ERROR

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/exception/custom/enums/UserErrorCode.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/exception/custom/enums/UserErrorCode.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements AppErrorCode {
     LOGIN_FAIL_ERROR("아이디 혹은 비밀번호를 다시 확인해주세요.", HttpStatus.BAD_REQUEST),
     USER_EMAIL_DUPLICATION_ERROR("아이디가 중복되었습니다.", HttpStatus.BAD_REQUEST),
+    NOT_FOUND_USER_BY_ID_ERROR("존재하지 않는 회원입니다.", HttpStatus.BAD_REQUEST),
     ;
 
     private final String message;
