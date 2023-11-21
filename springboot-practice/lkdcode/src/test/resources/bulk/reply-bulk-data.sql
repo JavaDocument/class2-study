67526d58067971273b3ab9a81b152d0a3f3c4084
lkdcode <dlrlejr1@naver.com>
2023-11-22 01:58:38 +0900

    Add [reply-bulk-data.sql] : reply-bulk-data.sql

---
+++ springboot-practice/lkdcode/src/test/resources/bulk/reply-bulk-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+    tb_reply (id, created_at, updated_at, content, post_id, user_id)
+VALUES
+    (1, now(), now(), '1번 제목입니다', 1, 1)
+    ,(2, now(), now(), '2번 제목입니다', 1, 2)
+    ,(3, now(), now(), '3번 제목입니다', 1, 3)
+    ,(4, now(), now(), '4번 제목입니다', 1, 4)
+    ,(5, now(), now(), '5번 제목입니다', 1, 5)
+    ,(6, now(), now(), '6번 제목입니다', 2, 6)
+    ,(7, now(), now(), '7번 제목입니다', 3, 7)
+    ,(8, now(), now(), '8번 제목입니다', 4, 8)
+    ,(9, now(), now(), '9번 제목입니다', 5, 9)
+    ,(10, now(), now(), '10번 제목입니다', 6, 10)
+    ;
