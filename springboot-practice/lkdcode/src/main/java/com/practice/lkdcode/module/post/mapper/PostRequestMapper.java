8848fce55fcad1bae1259c3c439f8f9fdaaa399f
lkdcode <dlrlejr1@naver.com>
2023-11-21 20:04:19 +0900

    Refactor [PostRequestMapper] : @Mapping(source, target) 순서로 컨벤션

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/mapper/PostRequestMapper.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/mapper/PostRequestMapper.java
@@ -10,15 +10,15 @@ import org.mapstruct.factory.Mappers;
 public interface PostRequestMapper {
     PostRequestMapper INSTANCE = Mappers.getMapper(PostRequestMapper.class);
 
-    @Mapping(source = "title", target = "title")
+    @Mapping(target = "title", source = "title")
     @Mapping(target = "user", ignore = true)
     Post createDTOToPost(PostRequestDTO.Create createDTO, @Context User user);
-
-    @Mapping(source = "title", target = "title")
-    Post updateDTOToPost(PostRequestDTO.Update updateDTO);
 
     @AfterMapping
     default void mapUser(@MappingTarget Post.PostBuilder postBuilder, @Context User user) {
         postBuilder.user(user);
     }
+
+    @Mapping(target = "title", source = "title")
+    Post updateDTOToPost(PostRequestDTO.Update updateDTO);
 }
