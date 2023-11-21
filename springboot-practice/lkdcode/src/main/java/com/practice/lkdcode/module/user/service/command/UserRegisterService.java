b007628020ffa602ef9da8e6b33301cfa2515de4
lkdcode <dlrlejr1@naver.com>
2023-11-21 18:00:00 +0900

    Add [UserRegisterService] : DTO Request, Response 분리

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/service/command/UserRegisterService.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/user/service/command/UserRegisterService.java
@@ -1,13 +1,15 @@
 package com.practice.lkdcode.module.user.service.command;
 
 import com.practice.lkdcode.global.config.jwt.JWTProvider;
-import com.practice.lkdcode.module.user.controller.dto.UserDTO;
+import com.practice.lkdcode.module.user.controller.dto.request.UserRequestDTO;
+import com.practice.lkdcode.module.user.controller.dto.response.UserResponseDTO;
 import com.practice.lkdcode.module.user.domain.User;
 import com.practice.lkdcode.module.user.domain.repository.UserRepository;
 import com.practice.lkdcode.module.user.exception.custom.UserEmailDuplicationException;
 import com.practice.lkdcode.module.user.exception.custom.UserLoginFailException;
 import com.practice.lkdcode.module.user.exception.custom.enums.UserErrorCode;
-import com.practice.lkdcode.module.user.mapper.UserMapper;
+import com.practice.lkdcode.module.user.mapper.UserRequestMapper;
+import com.practice.lkdcode.module.user.mapper.UserResponseMapper;
 import com.practice.lkdcode.module.user.service.UserRegisterUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,30 +17,28 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserRegisterService implements UserRegisterUsecase {
-    private static final UserMapper userMapper = UserMapper.INSTANCE;
     private final UserRepository userRepository;
     private final JWTProvider jwtProvider;
 
-
     @Override
-    public UserDTO.UserSignupResponseDTO executeUserSignup(UserDTO.UserSignupRequestDTO userSignupRequestDTO) {
+    public UserResponseDTO.UserSignupResponseDTO executeUserSignup(UserRequestDTO.UserSignupRequestDTO userSignupRequestDTO) {
         if (isExistsByEmail(userSignupRequestDTO.email())) {
             throw new UserEmailDuplicationException(UserErrorCode.USER_EMAIL_DUPLICATION_ERROR);
         }
 
-        User user = userMapper.signupDTOToUser(userSignupRequestDTO);
+        User user = UserRequestMapper.INSTANCE.signupDTOToUser(userSignupRequestDTO);
         User saved = userRepository.save(user);
 
-        return userMapper.userToUserSignupDTO(saved);
+        return UserResponseMapper.INSTANCE.userToUserSignupResponseDTO(saved);
     }
 
     @Override
-    public UserDTO.UserSignInResponseDTO executeUserSignin(UserDTO.UserSignInRequestDTO userSignInRequestDTO) {
+    public UserResponseDTO.UserSignInJwtResponseDTO executeUserSignin(UserRequestDTO.UserSignInRequestDTO userSignInRequestDTO) {
         User user = loadUserFrom(userSignInRequestDTO.email());
 
         if (user.getPassword().equals(userSignInRequestDTO.password())) {
             String jwt = jwtProvider.generateToken(user);
-            return new UserDTO.UserSignInResponseDTO(jwt);
+            return UserResponseMapper.INSTANCE.tokenToUserSignInJwtResponseDTO(jwt);
         }
 
         throw new UserLoginFailException(UserErrorCode.LOGIN_FAIL_ERROR);
