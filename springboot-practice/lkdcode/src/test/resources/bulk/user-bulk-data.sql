914167dfe867f9be6835dcea7431049d87b700f2
lkdcode <dlrlejr1@naver.com>
2023-11-22 01:58:28 +0900

    Add [user-bulk-data.sql] : user-bulk-data.sql

---
+++ springboot-practice/lkdcode/src/test/resources/bulk/user-bulk-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+    tb_user (id, email, password, user_status, created_at, updated_at)
+VALUES
+    (1, 'test1@test.com', 'password123', 'CREATED', now(), now())
+    ,(2, 'test2@test.com', 'password123', 'CREATED', now(), now())
+    ,(3, 'test3@test.com', 'password123', 'CREATED', now(), now())
+    ,(4, 'test4@test.com', 'password123', 'CREATED', now(), now())
+    ,(5, 'test5@test.com', 'password123', 'CREATED', now(), now())
+    ,(6, 'test6@test.com', 'password123', 'CREATED', now(), now())
+    ,(7, 'test7@test.com', 'password123', 'CREATED', now(), now())
+    ,(8, 'test8@test.com', 'password123', 'CREATED', now(), now())
+    ,(9, 'test9@test.com', 'password123', 'CREATED', now(), now())
+    ,(10, 'test10@test.com', 'password123', 'CREATED', now(), now())
+    ;
