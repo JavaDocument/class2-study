0287c2bc28ee934369e4689fb499de3d673c138a
lkdcode <dlrlejr1@naver.com>
2023-11-22 14:01:43 +0900

    Test.Refactor [ReplyResponseMapperTest] : BaseRepositoryList 를 상속하도록 수정

--- springboot-practice/lkdcode/src/test/java/com/practice/lkdcode/module/reply/mapper/ReplyResponseMapperTest.java
+++ springboot-practice/lkdcode/src/test/java/com/practice/lkdcode/module/reply/mapper/ReplyResponseMapperTest.java
@@ -1,10 +1,8 @@
 package com.practice.lkdcode.module.reply.mapper;
 
-import com.practice.lkdcode.module.post.domain.Post;
 import com.practice.lkdcode.module.reply.controller.dto.response.ReplyResponseDTO;
 import com.practice.lkdcode.module.reply.domain.Reply;
-import com.practice.lkdcode.module.user.domain.User;
-import com.practice.lkdcode.module.user.domain.status.UserStatus;
+import com.practice.lkdcode.support.base.BaseRepositoryList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,37 +10,13 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ReplyResponseMapperTest {
+class ReplyResponseMapperTest extends BaseRepositoryList {
     private static final ReplyResponseMapper MAPPER = ReplyResponseMapper.INSTANCE;
-    private static final String USER_EMAIL = "test@email.com";
-    private static final String USER_PASSWORD = "password123";
-    private static final String POST_TITLE = "게시글 제목입니다.";
-    private static final String POST_CONTENT = "게시글 내용입니다.";
-    private static final String REPLY_CONTENT = "댓글 내용입니다.";
-    private Post post;
-    private User user;
     private Reply reply;
 
     @BeforeEach
     void setEntity() {
-        this.user = User.builder()
-                .email(USER_EMAIL)
-                .password(USER_PASSWORD)
-                .userStatus(UserStatus.CREATED)
-                .build();
-
-        this.post = Post.builder()
-                .title(POST_TITLE)
-                .content(POST_CONTENT)
-                .user(user)
-                .build();
-
-        this.reply = Reply.builder()
-                .content(REPLY_CONTENT)
-                .post(post)
-                .user(user)
-                .build();
-
+        this.reply = super.replyRepository.findById(REPLY_ID).orElseThrow();
     }
 
     @Test
@@ -55,6 +29,14 @@ class ReplyResponseMapperTest {
         assertThat(create.content())
                 .isNotNull()
                 .isEqualTo(REPLY_CONTENT);
+
+        assertThat(create.userEmail())
+                .isNotNull()
+                .isEqualTo(USER_EMAIL);
+
+        assertThat(create.postId())
+                .isNotNull()
+                .isEqualTo(POST_ID);
     }
 
     @Test
@@ -67,6 +49,14 @@ class ReplyResponseMapperTest {
         assertThat(get.content())
                 .isNotNull()
                 .isEqualTo(REPLY_CONTENT);
+
+        assertThat(get.userEmail())
+                .isNotNull()
+                .isEqualTo(USER_EMAIL);
+
+        assertThat(get.postId())
+                .isNotNull()
+                .isEqualTo(POST_ID);
     }
 
     @Test
@@ -76,6 +66,19 @@ class ReplyResponseMapperTest {
         List<ReplyResponseDTO.Get> gets = MAPPER.replyListToGetDTO(List.of(reply, reply, reply, reply));
 
         // then
-        System.out.println(gets);
+
+        for (ReplyResponseDTO.Get get : gets) {
+            assertThat(get.content())
+                    .isNotNull()
+                    .isEqualTo(REPLY_CONTENT);
+
+            assertThat(get.userEmail())
+                    .isNotNull()
+                    .isEqualTo(USER_EMAIL);
+
+            assertThat(get.postId())
+                    .isNotNull()
+                    .isEqualTo(POST_ID);
+        }
     }
 }
