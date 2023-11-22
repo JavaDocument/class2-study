7e96209417f6ed6a8558b840cc1e134385fdcc4e
lkdcode <dlrlejr1@naver.com>
2023-11-22 13:56:22 +0900

    Refactor [PostResponseMapper] : @Mapping target, source 순서로 변경, replies 매핑 추가

--- springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/mapper/PostResponseMapper.java
+++ springboot-practice/lkdcode/src/main/java/com/practice/lkdcode/module/post/mapper/PostResponseMapper.java
@@ -2,8 +2,9 @@ package com.practice.lkdcode.module.post.mapper;
 
 import com.practice.lkdcode.module.post.controller.dto.response.PostResponseDTO;
 import com.practice.lkdcode.module.post.domain.Post;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import com.practice.lkdcode.module.reply.controller.dto.response.ReplyResponseDTO;
+import com.practice.lkdcode.module.reply.mapper.ReplyResponseMapper;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -12,21 +13,28 @@ import java.util.List;
 public interface PostResponseMapper {
     PostResponseMapper INSTANCE = Mappers.getMapper(PostResponseMapper.class);
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "user.email", target = "userEmail")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "userEmail", source = "user.email")
     PostResponseDTO.Create postToPostCreateDTO(Post post);
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "user.email", target = "userEmail")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "userEmail", source = "user.email")
     PostResponseDTO.Update postToPostUpdateDTO(Post post);
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "user.email", target = "userEmail")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "userEmail", source = "user.email")
+    @Mapping(target = "replies", ignore = true)
     PostResponseDTO.Get postToPostGetDTO(Post post);
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "user.email", target = "userEmail")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "userEmail", source = "user.email")
     PostResponseDTO.Delete postToPostDeleteDTO(Post post);
 
     List<PostResponseDTO.Get> postToPostListDTO(List<Post> posts);
+
+    @AfterMapping
+    default void mapReplies(@MappingTarget PostResponseDTO.Get.GetBuilder getDTOBuilder, Post post) {
+        List<ReplyResponseDTO.Get> replyList = ReplyResponseMapper.INSTANCE.replyListToGetDTO(post.getReplies());
+        getDTOBuilder.replies(replyList);
+    }
 }
