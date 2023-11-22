ab4d0c12639728db8e9db7412da0ef13309d2572
lkdcode <dlrlejr1@naver.com>
2023-11-22 14:18:13 +0900

    Test.Refactor [ReplyRequestMapperTest] : BaseRepositoryList 를 상속하도록 수정

--- springboot-practice/lkdcode/src/test/java/com/practice/lkdcode/module/reply/mapper/ReplyRequestMapperTest.java
+++ springboot-practice/lkdcode/src/test/java/com/practice/lkdcode/module/reply/mapper/ReplyRequestMapperTest.java
@@ -1,88 +1,68 @@
 package com.practice.lkdcode.module.reply.mapper;
 
+
 import com.practice.lkdcode.module.post.domain.Post;
 import com.practice.lkdcode.module.reply.controller.dto.request.ReplyRequestDTO;
 import com.practice.lkdcode.module.reply.domain.Reply;
 import com.practice.lkdcode.module.user.domain.User;
-import com.practice.lkdcode.module.user.domain.status.UserStatus;
+import com.practice.lkdcode.support.base.BaseRepositoryList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
-class ReplyRequestMapperTest {
+class ReplyRequestMapperTest extends BaseRepositoryList {
+    private static final String REPLY_CONTENT = "reply content";
+    private static final String REPLY_UPDATE_CONTENT = "reply update content";
     private static final ReplyRequestMapper MAPPER = ReplyRequestMapper.INSTANCE;
-    private static final String EMAIL = "test@email.com";
-    private static final String PASSWORD = "password123";
-    private static final String TITLE = "게시글 제목입니다.";
-    private static final String CONTENT = "게시글 내용입니다.";
     private Post post;
     private User user;
 
     @BeforeEach
     void setEntity() {
-        this.user = User.builder()
-                .email(EMAIL)
-                .password(PASSWORD)
-                .userStatus(UserStatus.CREATED)
-                .build();
-
-        this.post = Post.builder()
-                .title(TITLE)
-                .content(CONTENT)
-                .user(user)
-                .build();
+        this.user = userRepository.findById(USER_ID).orElseThrow();
+        this.post = postRepository.findById(POST_ID).orElseThrow();
     }
 
     @Test
-    void reply_create_dto_를_reply_entity_로_변환_성공_테스트() {
+    void createDTO_를_Entity_로_변환_성공_테스트() {
         // given
-        String replyContent = "댓글 내용입니다.";
-
-        ReplyRequestDTO.Create createRequestDTO = ReplyRequestDTO.Create.builder()
-                .content(replyContent)
+        ReplyRequestDTO.Create createDTO = ReplyRequestDTO.Create.builder()
+                .content(REPLY_CONTENT)
                 .build();
 
         // when
-        Reply reply = MAPPER.createDTOToReply(createRequestDTO, user, post);
+        Reply dtoToReply = MAPPER.createDTOToReply(createDTO, user, post);
 
         // then
-        assertThat(reply.getContent())
-                .isNotNull()
-                .isEqualTo(replyContent);
+        assertThat(dtoToReply.getContent())
+                .isEqualTo(REPLY_CONTENT);
 
-        assertThat(reply.getUser().getEmail())
-                .isNotNull()
-                .isSameAs(EMAIL);
+        assertThat(dtoToReply.getPost().getId())
+                .isEqualTo(POST_ID);
 
-        assertThat(reply.getPost().getTitle())
-                .isNotNull()
-                .isEqualTo(TITLE);
+        assertThat(dtoToReply.getUser().getEmail())
+                .isEqualTo(USER_EMAIL);
     }
 
     @Test
-    void reply_update_dto_를_reply_entity_로_변환_성공_테스트() {
+    void updateDTO_를_Entity_로_변환_성공_테스트() {
         // given
-        String updatedReplyContent = "수정된 내용입니다.";
-
-        ReplyRequestDTO.Update updateRequestDTO = ReplyRequestDTO.Update.builder()
-                .content(updatedReplyContent)
+        ReplyRequestDTO.Update updateDTO = ReplyRequestDTO.Update.builder()
+                .content(REPLY_UPDATE_CONTENT)
                 .build();
 
         // when
-        Reply reply = MAPPER.updateDTOToReply(updateRequestDTO, user, post);
+        Reply dtoToReply = MAPPER.updateDTOToReply(updateDTO, user, post);
 
         // then
-        assertThat(reply.getContent())
-                .isNotNull()
-                .isEqualTo(updatedReplyContent);
+        assertThat(dtoToReply.getContent())
+                .isEqualTo(REPLY_UPDATE_CONTENT);
 
-        assertThat(reply.getUser().getEmail())
-                .isNotNull()
-                .isSameAs(EMAIL);
+        assertThat(dtoToReply.getPost().getId())
+                .isEqualTo(POST_ID);
 
-        assertThat(reply.getPost().getTitle())
-                .isNotNull()
-                .isEqualTo(TITLE);
+        assertThat(dtoToReply.getUser().getEmail())
+                .isEqualTo(USER_EMAIL);
     }
 }
